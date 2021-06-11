24ebe5fbd6214cfd086ed2b08ba50365f2f75db2
sir-hp <77618213+sir-hp@users.noreply.github.com>
2021-06-11 12:54:39 +0200

    start on react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <>
+    <Welcome/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <>
+            <h2>Good morning (Insert Name Here)</h2>
+        </>
+    );
+}
+
+export default Welcome;
